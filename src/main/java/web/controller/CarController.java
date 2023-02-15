5307bd6a6ef64995ca1eaed70fbfdb44ccba158b
Bakulin Vladimir <wal1984@mail.ru>
2023-02-15 22:15:16 +0700

    GOOD

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -2,14 +2,13 @@ package web.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.service.CarService;
-import web.service.CarServiceImp;
-
 
 @Controller
 @RequestMapping("/cars")
